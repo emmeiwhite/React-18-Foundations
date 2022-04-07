a26fa0a39b735adc8b0ea48e1da34a107eefcc6e
Imran Rafiq Rather <emmeiwhite@gmail.com>
2022-04-07 15:26:26 +0530

    RF-6: Props, Destructuring, Re-Usuability

--- src/index.js
+++ src/index.js
@@ -3,37 +3,41 @@ import ReactDOM from "react-dom";
 
 // CSS
 import "./index.css";
-// Component
+
+// props as objects
+
+const book1 = {
+  author: "Geetanjali Shree, Daisy Rockwell",
+  image:
+    "	https://images-eu.ssl-images-amazon.com/images/I/41Hp+mTN4oL._AC_SX184_.jpg",
+  title: "Tomb of Sand",
+};
+
+const book2 = {
+  author: "S. Hussain Zaidi",
+  image:
+    "https://images-eu.ssl-images-amazon.com/images/I/51RFcsBf5jL._AC_SX184_.jpg",
+  title: "Zero Day",
+};
+
+// BookList Component
 function BookList() {
   return (
     <section className="booklist">
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+      <Book title={book1.title} author={book1.author} image={book1.image} />
+      <Book title={book2.title} author={book2.author} image={book2.image} />
     </section>
   );
 }
 
-const Book = () => {
+const Book = ({ title, author, image }) => {
   return (
     <article className="book">
-      <Image></Image>
-      <Title />
-      <Author />
+      <img src={image} alt={`Book by ${author}`} />
+      <h3>{title}</h3>
+      <p>{author}</p>
     </article>
   );
 };
 
-// Creating seperate Components
-const Image = () => (
-  <img
-    src="https://images-eu.ssl-images-amazon.com/images/I/51RFcsBf5jL._AC_SX184_.jpg"
-    alt="Book by Hussain Zaidi"
-  />
-);
-
-const Title = () => <h3>Zero Day</h3>;
-
-const Author = () => <p>S. Hussain Zaidi</p>;
 ReactDOM.render(<BookList />, document.querySelector("#root"));
