098bdbfbf5dc148f6863a707b5320ed352f865a8
Imran Rafiq Rather <emmeiwhite@gmail.com>
2022-04-07 14:44:29 +0530

    RF-4: BookList and Nesting of Components

--- src/index.js
+++ src/index.js
@@ -1,14 +1,37 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
 
 // Component
-function Greeting() {
+function BookList() {
   return (
-    <main>
-      <App />
-    </main>
+    <section>
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </section>
   );
 }
 
-ReactDOM.render(<Greeting />, document.querySelector("#root"));
+const Book = () => {
+  return (
+    <article>
+      <Image></Image>
+      <Title />
+      <Author />
+    </article>
+  );
+};
+
+// Creating seperate Components
+const Image = () => (
+  <img
+    src="https://images-eu.ssl-images-amazon.com/images/I/51RFcsBf5jL._AC_SX184_.jpg"
+    alt="Book by Hussain Zaidi"
+  />
+);
+
+const Title = () => <h3>Zero Day</h3>;
+
+const Author = () => <p>S. Hussain Zaidi</p>;
+ReactDOM.render(<BookList />, document.querySelector("#root"));
